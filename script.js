ba20b1a43f816034299fc1bb96d3e89ba98f0f4a
priyankasingh4 <99684288+priyankasingh4@users.noreply.github.com>
2026-03-05 17:33:45 -0800

    Change text displayed on canvas when condition met

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ function update() {
         ctx.fillStyle = "white";
         ctx.font = "40px Arial";
         ctx.textAlign = "center";
-        ctx.fillText("SCHOOL'S OUT! GO HOME!", canvas.width / 2, canvas.height / 2);
+        ctx.fillText("WARNING: BRAIN CELLS DELETING... GO STUDY!", canvas.width / 2, canvas.height / 2);
     }
 
     // Update Splatters
